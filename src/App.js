869a49fd8b6f8f29bc1bc192f742cb5a480ab1eb
Alek Kruk <kruk.aleksander@hotmail.com>
2020-12-29 18:01:13 +0100

    create all 4 components and import them to App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import './App.css';
+import Hero from './Hero';
+import Navbar from './Navbar';
+import Sidebar from './Sidebar';
+import Submenu from './Submenu';
 
 const App = () => {
   return <h1>Submenu project</h1>
