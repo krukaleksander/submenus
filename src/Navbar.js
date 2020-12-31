a21ab731ce8298836190ca18488ea381361509ea
Alek Kruk <kruk.aleksander@hotmail.com>
2020-12-31 07:23:05 +0100

    before handle menu in navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -12,7 +12,10 @@ const Navbar = () => {
     const bottom = tempBtn.bottom - 3;
     openSubmenu(page, { center, bottom });
   })
-  return <nav className='nav'>
+  const handleSubmenu = (e) => {
+
+  };
+  return <nav className='nav' onMouseOver={handleSubmenu}>
     <div className="nav-center">
       <div className="nav-header">
         <img src={logo} alt="stripe" className='nav-logo' />
